d0da827c6bb95d44101e923ff6d66c031f443965
ryu-UH2 <ryu@hydrogen.aero>
2021-09-04 17:52:43 -0700

    Added UH2 modals

--- raymond/scripts/ImageGallery.js
+++ raymond/scripts/ImageGallery.js
@@ -26,31 +26,64 @@ function showSlidesAlba(n) {
     var dots = document.getElementsByClassName("ALBA-dots");
     var captionText = document.getElementById("ALBA-caption");
 
-    console.log("Here10")
+    // console.log("Here10")
 
     if (n > slides.length) {
         slideIndexAlba = 1;
-        console.log("Here11");
+        // console.log("Here11");
     }
     if (n < 1) {
         slideIndexAlba = slides.length;
-        console.log("Here12");
+        // console.log("Here12");
     }
-    console.log("Here12.5");
+    // console.log("Here12.5");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        console.log("Here13");
+        // console.log("Here13");
     }
     console.log("Here13.5");
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
-        console.log("Here14");
+        // console.log("Here14");
     }
     slides[slideIndexAlba-1].style.display = "block";
     dots[slideIndexAlba-1].className += " active";
     captionText.innerHTML = dots[slideIndexAlba-1].alt;
-    console.log("Here15");
+    // console.log("Here15");
+}
+
+// UH2 project functions
+function modalGalleryDeployOnceUH2() {
+    showSlidesUH2(slideIndexUH2);
+}
+function plusSlidesUH2(n) {
+    showSlidesUH2(slideIndexUH2 += n);
+}
+function currentSlideUH2(n) {
+    showSlidesUH2(slideIndexUH2 = n);
+}
+function showSlidesUH2(n) {
+    var i;
+    var slides = document.getElementsByClassName("UH2-pics");
+    var dots = document.getElementsByClassName("UH2-dots");
+    var captionText = document.getElementById("UH2-caption");
+
+    if (n > slides.length) {
+        slideIndexUH2 = 1;
+    }
+    if (n < 1) {
+        slideIndexUH2 = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndexUH2-1].style.display = "block";
+    dots[slideIndexUH2-1].className += " active";
+    captionText.innerHTML = dots[slideIndexUH2-1].alt;
 }
 
 // Ravn project functions
